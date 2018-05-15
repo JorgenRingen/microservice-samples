03bb99b973b05c49edd8199a6aefdb1c7f5e51b1
Jorgen Ringen <jorgen.ringen@gmail.com>
2018-05-15 10:20:36 +0200

    added test folder

---
+++ tests/src/test/java/org/mssamples/CompaniesTest.java
@@ -0,0 +1,14 @@
+package org.mssamples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CompaniesTest {
+
+
+    @Test
+    void testMe() {
+        assertTrue(true);
+    }
+}
---
+++ tests/src/test/java/org/mssamples/EmployeesTest.java
@@ -0,0 +1,11 @@
+package org.mssamples;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeesTest {
+
+    @Test
+    void foo() {
+        System.out.println("EmployeesTest.foo");
+    }
+}
