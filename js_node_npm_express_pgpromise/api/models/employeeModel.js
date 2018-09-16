c64ab0d2da7c3a4b8dade25a583f516d1beb44ad
Erik Eiesland <erik@eies.land>
2018-09-16 21:51:20 +0200

    Removed left behind folder after rename

--- js_node_npm_express_pgpromise/api/models/employeeModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Employee {
-
-    constructor(id, firstname, lastname, dateOfBirth) {
-        this.id = id;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.dateOfBirth = dateOfBirth;
-    }
-};
